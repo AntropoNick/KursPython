02e3d4552f9d16b4567d7f060609e05c7371bb98
AntropoNick <dominik.reglinski@gmail.com>
2022-02-24 10:52:47 +0100

    wprowadzenie do pythona

---
+++ 01_Intro/BMI.py
@@ -0,0 +1,3 @@
+masa = float(input("Podaj mase w kg: "))
+wzrost= float(input("Podaj wzrost w m: "))
+print("Twoje BMI wynosi: ",round(masa/(wzrost**2),2))
---
+++ 01_Intro/weeks.py
@@ -0,0 +1,9 @@
+tydzien = 7
+dzien =24
+godzina =60
+print("Minuty w ciagu 7 tygodni =", 7*tydzien*dzien*godzina ,"minut")
+
+umiejetnosc =75
+GodzinyWTygodniu=4
+dzien =int(input("liczba dni"))
+print (dzien)
