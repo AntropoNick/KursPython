c08f135ceeb74fb6a095b2fcf2e7109f4a0c8808
AntropoNick <dominik.reglinski@gmail.com>
2022-02-28 16:40:13 +0100

    aktualizacja

---
+++ 02_Typy_zmiennych/2_1_binary_age.py
@@ -0,0 +1,5 @@
+# Na kartce papieru oblicz jak twój wiek będzie reprezentowany binarnie.
+# Sprawdź czy to samo zwróci Python.
+
+Age = int(input("Podaj wiek: "))
+print ("Twoj wiek w zapisie binarnymwiek to:", bin(Age))
---
+++ 02_Typy_zmiennych/2_2_binary_conv.py
@@ -0,0 +1,6 @@
+# Dla podanej liczby w systemie dwójkowym bin_num = 1001111 oblicz wartość w systemie dziesiętnym.
+# Zamianę z systemu binarnego na dziesiętny napisz samodzielnie (nie korzystaj z metody wbudowanej).
+
+bin_num = 1001111
+dec_num = (1*2**0)+(1*2**1)+(1*2**2)+(1*2**3)+(0*2**4)+(0*2**5)+(1*2**6)
+print("Wartość dzięsietna liczby binarnej ",bin_num," to ",dec_num," co potwierda wbudowana funkcja dla której wartość dziesiętna również wynosi:", 0b1001111)
